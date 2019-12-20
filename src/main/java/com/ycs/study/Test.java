8566a6490b5398cbb3069b9e20505a16e070dee7
chengshuoyu <798936856@qq.com>
2019-12-20 10:37:05 +0800

    test

---
+++ src/main/java/com/ycs/study/Test.java
@@ -0,0 +1,12 @@
+package com.ycs.study;
+
+/**
+ * <p>com.ycs.study</p>
+ * <p>ClassName: Test</p>
+ * <p>Description: TODO</p>
+ * <p>Author: Chengshuo Yu</p>
+ * <p>Date: 2019/12/20 10:36</p>
+ * <p>Version: v1.0</p>
+ */
+public class Test {
+}
